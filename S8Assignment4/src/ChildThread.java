a4c4e59a57f2d2641c17632906a90159905d91a3
PavanAv <pavanaluri2011@gmail.com>
2016-10-26 15:17:02 +0530

    Session8Assignment4
    Session8Assignment4

---
+++ S8Assignment4/src/ChildThread.java
@@ -0,0 +1,19 @@
+public class ChildThread extends Thread{
+	public ChildThread(){		
+		System.out.println("my thread created " + this);			
+		}						
+		@Override			
+		public void run() {				
+		super.run();				
+		for (int count = 0; count < 10; count++) {					
+		System.out.println("Printing the count " + count);					
+		try {						
+		Thread.sleep(1000);					
+		} 
+		catch (InterruptedException e) {						
+		e.printStackTrace();					
+		}				
+		}				
+		System.out.println("Child thread's run is over");			
+		}
+}
